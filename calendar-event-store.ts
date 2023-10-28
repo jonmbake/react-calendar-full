30febae30cddc34ac65bfcef0133e4826ff17323
Jon Bake <jonmbake@gmail.com>
2023-10-28 14:39:00 -0500

    Add calendar event store listeners

--- calendar-event-store.ts
+++ calendar-event-store.ts
@@ -8,35 +8,63 @@ export type CalendarEvent = {
   endTime: ISO8601TimeString;
 };
 
-class CalendarEventStore {
-  events: Array<CalendarEvent>;
+type CalendarEventListener = (event: CalendarEvent) => void;
+
+export class CalendarEventStore {
+  public events: Array<CalendarEvent> = [];
+  private addListeners: Array<CalendarEventListener> = [];
+  private updateListeners: Array<CalendarEventListener> = [];
+  private deleteListeners: Array<CalendarEventListener> = [];
 
   constructor(events: Array<CalendarEvent>) {
     this.events = events;
   }
 
-  public add(event: CalendarEvent) {
+  public add(event: CalendarEvent): void {
     this.events = [...this.events, event];
+    this.notify(this.addListeners, event);
   }
 
-  public addOrUpdate(event: CalendarEvent) {
+  public addOrUpdate(event: CalendarEvent): void {
     if (event.id == null) {
       event.id = Math.max(...this.events.map((event) => event.id || 0)) + 1;
-      this.events.push(event);
-      return;
-    }
-
-    const index = this.events.findIndex((e) => e.id === event.id);
-
-    if (index !== -1) {
-      this.events[index] = event;
-    } else {
       this.add(event);
+    } else {
+      const index = this.events.findIndex((e) => e.id === event.id);
+      if (index !== -1) {
+        this.events[index] = event;
+        this.notify(this.updateListeners, event);
+      } else {
+        this.add(event);
+      }
     }
   }
 
-  public delete(eventId: number) {
+  public delete(eventId: number): void {
+    const event = this.events.find((event) => event.id !== eventId);
     this.events = this.events.filter((event) => event.id !== eventId);
+    if (event) {
+      this.notify(this.deleteListeners, event);
+    }
+  }
+
+  public onAdd(listener: CalendarEventListener): void {
+    this.addListeners.push(listener);
+  }
+
+  public onUpdate(listener: CalendarEventListener): void {
+    this.updateListeners.push(listener);
+  }
+
+  public onDelete(listener: CalendarEventListener): void {
+    this.deleteListeners.push(listener);
+  }
+
+  private notify(
+    listeners: Array<CalendarEventListener>,
+    item: CalendarEvent,
+  ): void {
+    listeners.forEach((listener) => listener(item));
   }
 }
 
